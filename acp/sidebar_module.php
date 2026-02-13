5743de65acf5e8dc943d32acd121c1f547aa9cc7
Fred Rimbert <62480197+ssl-origin@users.noreply.github.com>
2026-02-13 17:28:14 +0100

    Update sidebar_module.php

--- acp/sidebar_module.php
+++ acp/sidebar_module.php
@@ -1,4 +1,4 @@
-<?php
+<?php<?php
 /**
 *
 * @package phpBB Extension - phpBB Sidebar
@@ -11,41 +11,38 @@ namespace caforum\sidebar\acp;
 
 class sidebar_module
 {
-	public $u_action;
+    public $u_action;
 
-	function main($id, $mode)
-	{
-		global $config, $request, $template, $user;
+    function main($id, $mode)
+    {
+        global $config, $request, $template, $user;
 
-		$user->add_lang_ext('caforum/sidebar', 'common');
+        $user->add_lang_ext('caforum/sidebar', 'common');
 
-		$this->tpl_name = 'acp_sidebar';
-		$this->page_title = 'SIDEBAR_SETTINGS';
+        $this->tpl_name = 'acp_sidebar';
+        $this->page_title = 'SIDEBAR_SETTINGS';
 
-		add_form_key('caforum_sidebar');
+        add_form_key('caforum_sidebar');
 
-		if ($request->is_set_post('submit'))
-		{
-			if (!check_form_key('caforum_sidebar'))
-			{
-				trigger_error('FORM_INVALID');
-			}
+        if ($request->is_set_post('submit'))
+        {
+            if (!check_form_key('caforum_sidebar')) trigger_error('FORM_INVALID');
 
-			for ($i = 1; $i <= 5; $i++)
-			{
-				set_config("sidebar_link{$i}_name", $request->variable("sidebar_link{$i}_name", '', true));
-				set_config("sidebar_link{$i}_url", $request->variable("sidebar_link{$i}_url", '', true));
-			}
+            for ($i = 1; $i <= 5; $i++)
+            {
+                set_config("sidebar_link{$i}_name", $request->variable("sidebar_link{$i}_name", '', true));
+                set_config("sidebar_link{$i}_url", $request->variable("sidebar_link{$i}_url", '', true));
+            }
 
-			trigger_error($user->lang('SIDEBAR_SAVED') . adm_back_link($this->u_action));
-		}
+            trigger_error($user->lang('SIDEBAR_SAVED') . adm_back_link($this->u_action));
+        }
 
-		for ($i = 1; $i <= 5; $i++)
-		{
-			$template->assign_vars([
-				"SIDEBAR_LINK{$i}_NAME" => $config["sidebar_link{$i}_name"] ?? '',
-				"SIDEBAR_LINK{$i}_URL"  => $config["sidebar_link{$i}_url"] ?? '',
-			]);
-		}
-	}
+        for ($i = 1; $i <= 5; $i++)
+        {
+            $template->assign_vars([
+                "SIDEBAR_LINK{$i}_NAME" => $config["sidebar_link{$i}_name"] ?? '',
+                "SIDEBAR_LINK{$i}_URL"  => $config["sidebar_link{$i}_url"] ?? '',
+            ]);
+        }
+    }
 }
