05e858badd6c16e83b823d9515b5804ec5e80f75
Fred Rimbert <62480197+ssl-origin@users.noreply.github.com>
2026-02-13 17:23:48 +0100

    Create install_sidebar_config.php

---
+++ migrations/install_sidebar_config.php
@@ -0,0 +1,31 @@
+<?php<?php
+/**
+*
+* @package phpBB Extension - phpBB Sidebar
+* @copyright (c) 2024 Fred Rimbert https://forums.caforum.fr/
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace caforum\sidebar\migrations;
+
+class install_sidebar_config extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['sidebar_link1_name']);
+	}
+
+	public function update_data()
+	{
+		$data = [];
+
+		for ($i = 1; $i <= 5; $i++)
+		{
+			$data[] = ['config.add', ["sidebar_link{$i}_name", '']];
+			$data[] = ['config.add', ["sidebar_link{$i}_url", '']];
+		}
+
+		return $data;
+	}
+}
